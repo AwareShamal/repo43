ce4261549cfae3957808338c89775c0bf8cac26f
AwareShamal <121937255+AwareShamal@users.noreply.github.com>
2026-04-28 19:48:50 +0530

    Add greeting for everyone in HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Shamal! How are you?");
+        System.out.println("Hello Everyone, How are You? What is going on?");
     }
 }
